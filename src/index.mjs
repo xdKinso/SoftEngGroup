f9a14a6f99a0de857e5ca3d62e6034b9f6ca5490
Krystian Mowinski <124276188+xdKinso@users.noreply.github.com>
2023-03-09 14:34:32 +0000

    index

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,21 @@
+
 /* Import dependencies */
+/* const express = require("express");
+const mysql = require("mysql2/promise"); */
 import express from "express";
 import mysql from "mysql2/promise";
 
 /* Create express instance */
 const app = express();
 const port = 3000;
+
+// Integrate Pug with Express
+app.set("view engine", "pug");
+
+// Serve assets from 'static' folder
+app.use(express.static("static"));
+
+console.log(process.env.NODE_ENV);
 
 /* Setup database connection */
 const db = await mysql.createConnection({
@@ -16,7 +27,7 @@ const db = await mysql.createConnection({
 
 /* Landing route */
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.render("index");
 });
 
 // Sample API route
@@ -24,8 +35,34 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
-// Returns an array of cities from the database
+// Landing route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Gallery route
+app.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
+
+// About route
+app.get("/about", (req, res) => {
+  res.render("about", { title: "Boring about page" });
+});
+
 app.get("/cities", async (req, res) => {
+  try {
+    // Fetch cities from database
+    const [rows, fields] = await db.execute("SELECT * FROM `city`");
+    /* Render cities.pug with data passed as plain object */
+    return res.render("cities", { rows, fields });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// Returns JSON array of cities
+app.get("/api/cities", async (req, res) => {
   const [rows, fields] = await db.execute("SELECT * FROM `city`");
   return res.send(rows);
 });
